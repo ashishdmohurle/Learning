fe051b2025a2a2171a50eefc9a291cf7a26600b3
ashishdmohurle <143335432+ashishdmohurle@users.noreply.github.com>
2024-01-17 15:40:11 +0530

    Update App.java

--- InfosysAssignment/src/main/java/Test/Test/App.java
+++ InfosysAssignment/src/main/java/Test/Test/App.java
@@ -6,6 +6,7 @@ package Test.Test;
  */
 public class App 
 {
+    String password = AshishQwwrwqe2344
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
